8c5b4cb8b3382542fa393dbb989eecb2a6b1403e
ccat3z <c0ldcat3z@gmail.com>
2019-11-19 08:57:33 +0800

    realize project

--- Src/main.c
+++ Src/main.c
@@ -1,33 +1,19 @@
 #include "system/sys.h"
 #include "system/delay.h"
 #include "system/usart.h"
-void Delay(__IO uint32_t nCount);
-void Delay(__IO uint32_t nCount) {
-    while (nCount--) {
-    }
-}
+#include "hardware/exti.h"
 
 int main(void) {
-    GPIO_InitTypeDef GPIO_Initure;
-
-    Cache_Enable();                  //打开L1-Cache
-    HAL_Init();                      //初始化HAL库
-    Stm32_Clock_Init(432, 25, 2, 9); //设置时钟,216Mhz
-    __HAL_RCC_GPIOB_CLK_ENABLE();    //开启GPIOB时钟
-
-    GPIO_Initure.Pin = GPIO_PIN_0 | GPIO_PIN_1; // PB1,0
-    GPIO_Initure.Mode = GPIO_MODE_OUTPUT_PP;    //推挽输出
-    GPIO_Initure.Pull = GPIO_PULLUP;            //上拉
-    GPIO_Initure.Speed = GPIO_SPEED_HIGH;       //高速
-    HAL_GPIO_Init(GPIOB, &GPIO_Initure);
-
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
-    Delay(0x1FFFFFF);
-
-    while (1) {
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-        Delay(0x1FFFFFF);
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_1);
-    }
+    Cache_Enable();                 //打开L1-Cache
+    HAL_Init();				        //初始化HAL库
+    Stm32_Clock_Init(432,25,2,9);   //设置时钟,216Mhz 
+    delay_init(216);                //延时初始化
+	uart_init(115200);		        //串口初始化
+    LED_Init();                     //初始化LED
+    EXTI_Init();                    //外部中断初始化
+    while(1)
+    {
+        printf("OK\r\n");           //打印OK提示程序运行
+        delay_ms(1000);             //每隔1s打印一次 
+	} 
 }
